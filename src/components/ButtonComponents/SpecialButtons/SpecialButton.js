3d61b6df64e9cbabb0ccef975629cc7099c9f219
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-06 16:22:56 -0500

    added styling to buttons

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -6,7 +6,7 @@ const SpecialButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>{props.text}</button>
+      <button className='specialButt'>{props.text}</button>
     </>
   );
 };
